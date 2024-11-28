05d704af71c11dba2f107d317f83e027ab26c9ec
claudiojas <claudio.soares@lacreisaude.com.br>
2024-11-27 22:41:39 -0300

    fix: corrige tag

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -11,28 +11,28 @@ function App() {
   const { formDataLocation, setFormDataLocation } = useContext(ContextApi);
 
   return(
-    <>
-    <Container>
-      <div className="flex flex-col w-full h-full justify-around p-6 gap-2" >
-        <h1 className="text-center text-2xl font-semibold">Para onde você quer ir?</h1>
-        <div className="flex flex-col gap-2 flex-1 w-full">
-          <SubContainer variant="overflow-y-auto">
-              {!formDataLocation?.userId ? (
-                <Form func={setFormDataLocation} />
-              ) : (
-                <DriverList />
-              )}
-              {formDataLocation?.userId && 
-              <div className="w-full flex justify-end">
-                <Button labelName="retornar" funClick={()=>setFormDataLocation(undefined)}/>
-              </div>}
-          </SubContainer>
+    <div>
+      <Container>
+        <div className="flex flex-col w-full h-full justify-around p-6 gap-2" >
+          <h1 className="text-center text-2xl font-semibold">Para onde você quer ir?</h1>
+          <div className="flex flex-col gap-2 flex-1 w-full">
+            <SubContainer variant="overflow-y-auto">
+                {!formDataLocation?.userId ? (
+                  <Form func={setFormDataLocation} />
+                ) : (
+                  <DriverList />
+                )}
+                {formDataLocation?.userId && 
+                <div className="w-full flex justify-end">
+                  <Button labelName="retornar" funClick={()=>setFormDataLocation(undefined)}/>
+                </div>}
+            </SubContainer>
 
+          </div>
         </div>
-      </div>
       </Container>
       <MapPage />
-    </>
+    </div>
   )
 }
 
